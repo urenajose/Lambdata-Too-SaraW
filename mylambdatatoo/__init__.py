09767181efe8f21ebf7aa5c5adb84bd46083558f
SaraWestWA <SaraEWestDS@gmail.com>
2020-10-29 23:29:52 -0700

    null function improved

--- mylambdatatoo/__init__.py
+++ mylambdatatoo/__init__.py
@@ -1,3 +1,4 @@
 __version__ = '0.0.2'.
 my_string = 'This is my_sting from __init__.py'
 
+import .ds_utilities
--- mylambdatatoo/ds_utilities.py
+++ mylambdatatoo/ds_utilities.py
@@ -1,46 +1,96 @@
 import pandas as pd
 import numpy as np
+import warnings
 
-def clean_frame(df):
-	'''
-	Parameter is a dataframe with numerical and/or categorical data
-
-	Function returns the dataframe with leading and trailing zeros removed,
-	'?', '', and empty cells replaced with NaN and dtype is float, if possible.
-	'''
-
-	df = df.applymap(lambda x: x.strip() if type(x) == str else x)
-	df = df.applymap(lambdax: np.nan if type(x) == str and x =='' or
-			 x == None or x == '?' else x)
-	df.apply(pd.to_numeric, errors = 'ignore')
-	return(df)
-
-def null_counts(df):
-
+class My_Ready_Frame():
+    def __init__(self, df):
+        self.df = df
     """
-    Param is a dataframe, can have both catagorical and numeric data
-    Function returns a dataframe of counts for  null values and 0.
+    Param df = dataframe, can have both catagorical and numeric data
     """
-    # surpress warning comparing pd objects to np.nan
-    # warnings.simplefilter(action='ignore', category=FutureWarning)
-    df = df.applymap(lambda x: x.strip() if type(x) == str else x)
-    columns = list(df.columns)
-    n_c = []
-    z_c = []
-    q_c = []
-    m_c = []
-    for i in columns:
-        nan_count = df[i].isnull().sum()
-        zero_count = (sum(df[i] == '0')+sum(df[i] == 0))
-        q_mark_count = sum(df[i] == '?')
-        missing_count = (sum(df[i] == '')+sum(df[i] == None))
+
+    def clean_frame(self):
+        
+        """
+        Param is a dataframe, can have both catagorical and numeric data
+
+        Method returns the dataframe with leading and trailing zeros removed;
+        '?','', and empty cells replaced with NaN, dtype changed to float
+        if possible.
+        """
+
+        df = self.applymap(lambda x: x.strip() if type(x) == str else x)
+        df = df.applymap(lambda x: np.nan if type(x) == str and x == '' or
+                        x == None or x == '?' else x)
+        df = df.apply(pd.to_numeric, errors='ignore')
+        return (df)
+
+
+
+
+    def null_counts(self, npnan, zero, qmark, missing):
+
+        """
+        Param (df = dataframe, npnan=True(default),zero=True(default),
+        qmark=True(default), missing=True(default)).
+
+        Method returns a dataframe of counts for specified null values and 0,
+        default is all.
+        """
+
+  # surpress warning comparing pd objects to np.nan
+  warnings.simplefilter(action='ignore', category=FutureWarning)
+  dfn = df.applymap(lambda x: x.strip() if type(x) == str else x)
+  columns = list(df.columns)
+  options = [npnan, zero, qmark, missing]
+  null_name = ['NaN', 0, '?', 'Missing']
+  keeper = []
+  index = []
+
+  for p in range(4):
+    if options[p] == True:
+      index.append(null_name[p])
+      n_c = []
+      for i in columns:
+        null_function = [
+                         dfn[i].isnull().sum(),
+                         (sum(dfn[i] == '0')+sum(dfn[i] == 0)),
+                         sum(dfn[i] == '?'),
+                         (sum(dfn[i] == '')+sum(dfn[i] == None))
+                         ]
+        nan_count = null_function[p]
         n_c.append(nan_count)
-        z_c.append(zero_count)
-        q_c.append(q_mark_count)
-        m_c.append(missing_count)
-    null_count = pd.DataFrame(data=(n_c, z_c, q_c, m_c),
-                            index=['NaN', 0, '?', 'Missing'],
-                            columns=columns)
-    return (null_count)
+      keeper.append(n_c)
+
+  null_count = pd.DataFrame(data=keeper, index=index, columns=columns)
+  return (null_count)
 
 
+
+if __name__ == '__main__':
+
+# Setup DataFrame to test My_Ready_Frame methods
+
+    data = ([[1, '', 1, 4, np.nan, 6, '0', 2],
+            [2, 2, 1, 0, 1, 6, 6, 2],
+            ['0', 'x', '? ', 'x', '  ', 0, 'x'],
+            [np.nan, '?', '? ', 'c ', ' x', ' 0 ', ],
+            [.4, .5, .35, ' ?', np.nan, .55, ],
+            [5, .55, 0, .5, .2, .4, .0, .6]
+            ])
+
+    names = ['int_a', 'int_b', 'str_a', 'str_b', 'fl_a', 'fl_b']
+
+    df = pd.DataFrame(data, index=names).T
+
+    print(df)
+
+#Simple Test for null_counts method
+    z = My_Ready_Frame.null_counts(df, True, True, True, True)
+    print(z)
+    print('')
+
+#Simple Test for clean_frame method
+    q = My_Ready_Frame.clean_frame(df)
+    print(q)
+    print('')
